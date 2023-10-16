fb356d21b3811b0391834bd84ca275d4814e5a0d
Vladislav Pysarenko <vlad@pysarenko.com>
2023-10-16 14:59:27 +0200

    add some package files

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
+from pathlib import Path
 from setuptools import setup, find_namespace_packages
 
-setup(name='GoIT NEO Python HW 1',
+long_description = Path("README.md").read_text()
+
+setup(name='goitneo-python-hw-1-group5',
       version="0.0.1",
       description="GoIT NeoVersity Python Homework 1",
+      long_description=long_description,
       url="https://github.com/AegisVP/goitneo-python-hw-1-group5",
       author='Vladyslav Pysarenko',
       author_email='vlad@pysarenko.com',
       license='MIT',
       packages=find_namespace_packages(),
+      classifiers=["Programming Language :: Python :: 3"],
       install_requires=['faker'],
+      package_data={"":["*.json"]},
+      include_package_data=False,
       entry_points={'console_scripts': [
           'run_work1 = assignment1.main:run_code',
           'run_work2 = assignment2.main:run_code'
